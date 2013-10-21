374b3cd87321162714942f2dbe8421f181e0577d
Michael Ballard <michael@ballard.is>
2013-10-21 18:06:34 -0400

    implement mailchimp form

--- javascripts/badgekit.js
+++ javascripts/badgekit.js
@@ -14,22 +14,21 @@ $('#emsubmit').click(function(){
 
 $('.notifywrap').stop().animate({ bottom: '0px' },250);
 
+});
 
-  //a function to create an alert box element and add to the DOM
-  function makeAlert(text,status) {
-    if($('.alert-box').length != 0) {
-      $('.alert-box').remove();
-    }
-    var alert = '<div data-alert class="alert-box ' + status + '"><span class="content">' + text + '</span><a href="#" class="close">&times;</a></div>';
-    $(alert).prependTo($('body')).fadeIn('fast');
+//a function to create an alert box element and add to the DOM
+function makeAlert(text,status) {
+  if($('.alert-box').length != 0) {
+    $('.alert-box').remove();
   }
+  var alert = '<div data-alert class="alert-box ' + status + '"><span class="content">' + text + '</span><a href="#" class="close">&times;</a></div>';
+  $(alert).prependTo($('body')).fadeIn('fast');
+}
 
   function makeReveal(content){
-  	var reveal = $('<div id="reveal" class="reveal-modal"></div>');
-  	reveal.append(content).appendTo('body').foundation('reveal','open');
+    var reveal = $('<div id="reveal" class="reveal-modal"></div>');
+    reveal.append(content).appendTo('body').foundation('reveal','open');
   }
-
-});
 
 $(document).scroll(function(){
 
