10c5581695a3519437f0890d0760f40d4eaf5272
Erik Christensen <christensenep@gmail.com>
2014-01-08 22:06:48 -0800

    Adding directory pages

--- app/views/directory.js
+++ app/views/directory.js
@@ -2,41 +2,50 @@ const openbadger = require('../lib/openbadger');
 const Badge = require('../models/badge')("DATABASE");
 const async = require('async');
 const middleware = require('../middleware');
+const url = require('url');
 
 const PAGE_SIZE = 12;
 
 exports.home = function home (req, res, next) {
-  const pageNum = parseInt(req.query.page) || 1;
-
-  async.parallel({
-    active: function (callback) {
-      openbadger.getAllBadges(function (err, data) {
-        callback(err, data);
-      });
-    },
-    inactive: function (callback) {
-      Badge.get({}, function (err, rows) {
-        callback(err, rows);
-      });
-    }
-  },
-  function (err, results) {
+  function handleResults (err, badges) {
     if (err)
       return res.send(500, err);
-
-    var badges = results.inactive.concat(results.active.badges);
 
     const startIndex = (pageNum-1) * PAGE_SIZE;
     const pages = Math.ceil(badges.length / PAGE_SIZE);
 
     badges = badges.slice(startIndex, startIndex + PAGE_SIZE);
 
-    return res.render('directory/home.html', { badges: badges, page: pageNum, pages: pages });
-  });
+    return res.render('directory/home.html', { badges: badges, page: pageNum, pages: pages, category: category });
+  }
+
+  const pageNum = parseInt(req.query.page) || 1;
+  const category = req.query.category || 'draft';
+
+  switch (category) {
+    case 'published':
+      openbadger.getAllBadges(function (err, data) {
+        handleResults(err, data.badges);
+      });
+      break;
+    case 'archived':
+      // openbadger does not yet have a concept of archived badges
+      handleResults(null, []);
+      break;
+    case 'template':
+      Badge.get({ status: 'template' }, handleResults);
+      break;
+    default:
+      Badge.get({ status: 'draft' }, handleResults);
+      break;
+  }
 };
 
 exports.addBadge = function addBadge (req, res, next) {
-  Badge.put({ name: 'New Badge', status: 'draft' }, function (err, result) {
-    return middleware.redirect('directory', 302)(req, res, next);
+  const category  = req.query.category || 'draft';
+
+  Badge.put({ name: 'New Badge', status: category }, function (err, result) {
+    var directoryUrl = res.locals.url('directory') + '?category=' + category;
+    return middleware.redirect(directoryUrl, 302)(req, res, next);
   });
 };
