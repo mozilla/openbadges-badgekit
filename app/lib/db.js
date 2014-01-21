a256834a70206dc873c7f24d0d386d99c1443987
Erik Christensen <christensenep@gmail.com>
2014-01-20 21:02:46 -0800

    Removing database reconnect hack

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,7 +2,6 @@ const path = require('path');
 const fs = require('fs');
 const streamsql = require('streamsql');
 const config = require('./config');
-const mysql = require('mysql');
 
 function getDbConfig (prefix) {
   prefix += '_';
@@ -16,36 +15,7 @@ function getDbConfig (prefix) {
 }
 
 function getDb (prefix) {
-  var db = streamsql.connect(getDbConfig(prefix));
-
-  function handleDisconnect() {
-    db.connection = mysql.createConnection(getDbConfig(prefix));
-    db.connection.connect(function(err) {
-      if(err) {
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000);
-      }
-      db.query = db.driver.getQueryFn(db.connection);
-      db.queryStream = db.driver.getStreamFn(db.connection);
-    });   
-
-    setErrorHandler();
-  }
-
-  function setErrorHandler() {
-    db.connection.on('error', function(err) {
-      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        handleDisconnect();
-      }
-      else {
-        console.error(err);
-      }
-    });
-  }
-
-  setErrorHandler();
-
-  return db;
+  return streamsql.connect(getDbConfig(prefix));
 };
 
 module.exports.getDb = getDb;
