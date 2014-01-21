17cd2bbc74b3a2f5f84102eb798c2d24958b3267
Erik Christensen <christensenep@gmail.com>
2014-01-21 08:03:34 -0800

    Tweaks for staging site

--- app/lib/db.js
+++ app/lib/db.js
@@ -2,6 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const streamsql = require('streamsql');
 const config = require('./config');
+const mysql = require('mysql');
 
 var dbs = {};
 
@@ -19,10 +20,37 @@ function getDbConfig (prefix) {
 function getDb (prefix) {
   if (!(prefix in dbs)) {
     dbs[prefix] = streamsql.connect(getDbConfig(prefix));
+
+    function handleDisconnect() {
+      db.connection = mysql.createConnection(getDbConfig(prefix));
+      db.connection.connect(function(err) {
+        if(err) {
+          console.log('error when connecting to db:', err);
+          setTimeout(handleDisconnect, 2000);
+        }
+        db.query = db.driver.getQueryFn(db.connection);
+        db.queryStream = db.driver.getStreamFn(db.connection);
+      });   
+
+      setErrorHandler();
+    }
+
+    function setErrorHandler() {
+      db.connection.on('error', function(err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+          handleDisconnect();
+        }
+        else {
+          console.error(err);
+        }
+      });
+    }
+
+    setErrorHandler();
   }
 
   return dbs[prefix];
-}
+};
 
 module.exports.getDb = getDb;
 module.exports.getDbConfig = getDbConfig;
