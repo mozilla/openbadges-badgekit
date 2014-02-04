78514f63fc63df61bf9136066b8ccda1043fd503
Zahra Jabini <zahra.arch@gmail.com>
2014-02-04 13:55:15 -0500

    fixes made post rebase

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
-  var leftWhatIsContainer = $('.home-what-is-badge');
-  var rightWhatIsContainer = $('.home-what-is-image');
+// im not sure this is necessary right now, we'll replace the image
+// with the exact one jess will provide and it wont need to resized.
+// commenting out for now?
 
-  function resizeWhatIsContainer() {
-    rightWhatIsContainer.height(leftWhatIsContainer.height());
-  }
+// $(document).ready(function() {
+//   var leftWhatIsContainer = $('.home-what-is-badge');
+//   var rightWhatIsContainer = $('.home-what-is-image');
 
-  leftWhatIsContainer.resize(resizeWhatIsContainer);
-  resizeWhatIsContainer();
-});
+//   function resizeWhatIsContainer() {
+//     rightWhatIsContainer.height(leftWhatIsContainer.height());
+//   }
+
+//   leftWhatIsContainer.resize(resizeWhatIsContainer);
+//   resizeWhatIsContainer();
+// });
