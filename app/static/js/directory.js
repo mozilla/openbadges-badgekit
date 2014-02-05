75bcecce4ef1fbfcbfec9b6824b1f7f7d5ef9fbd
Erik Christensen <christensenep@gmail.com>
2014-02-05 15:46:52 -0800

    Fixed disappearing sort menu

--- app/static/js/directory.js
+++ app/static/js/directory.js
@@ -1,15 +1,4 @@
 $(document).ready(function() {
-  var dropdownNavButton = $('.dropdown-nav');
-
-  dropdownNavButton.click(function() {
-    var navButtonOffset = dropdownNavButton.offset();
-    var dropdownMenu = $('.sort-dropdown');
-    dropdownMenu.toggle();
-    dropdownMenu.offset( { top: navButtonOffset.top + dropdownNavButton.height(),left: navButtonOffset.left } );
-
-    return false;
-  });
-
   var actionButton = $('.action-btn');
   actionButton.click(function(e) {
     var target = $(e.target);
@@ -24,7 +13,6 @@ $(document).ready(function() {
   });
 
   $(document).click(function() {
-    $('.sort-dropdown').hide();
     $('.action-dropdown').hide();
   });
 });
