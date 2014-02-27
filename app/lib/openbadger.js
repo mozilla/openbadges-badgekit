2b57822e9aaf6e74f107a462d889b006cc6c970d
Erik Christensen <christensenep@gmail.com>
2014-02-27 13:12:04 -0500

    Compatibility with new badge model

--- app/lib/openbadger.js
+++ app/lib/openbadger.js
@@ -9,9 +9,16 @@ module.exports.toBadgekitBadge = function toBadgekitBadge(badge) {
   newBadge.id = badge.slug;
   newBadge.description = badge.strapline;
   newBadge.name = badge.name;
-  newBadge.earnerDescription = badge.description;
-  newBadge.consumerDescription = badge.description;
+  newBadge.earnerDescription = badge.earnerDescription;
+  newBadge.consumerDescription = badge.consumerDescription;
+  newBadge.rubricUrl = badge.rubricUrl;
+  newBadge.timeValue = badge.timeValue;
+  newBadge.timeUnits = badge.timeUnits;
+  newBadge.limit = badge.limit;
+  newBadge.unique = badge.unique;
   newBadge.imageUrl = badge.imageUrl;
+  newBadge.issuerUrl = badge.issuerUrl;
+  newBadge.criteria = badge.criteria;
 
   return newBadge;
 };
@@ -23,7 +30,19 @@ module.exports.toOpenbadgerBadge = function toOpenbadgerBadge(badge) {
   newBadge.strapline = badge.description || ' ';
   // openbadger-issue-client doesn't yet support uploading an image file, so for now we're keeping the images in the badgekit db
   newBadge.imageUrl = config('PERSONA_AUDIENCE') + '/images/badge/' + badge.id;
-  newBadge.description = badge.earnerDescription || newBadge.strapline;
-
+  newBadge.earnerDescription = badge.earnerDescription;
+  newBadge.consumerDescription = badge.consumerDescription;
+  newBadge.rubricUrl = badge.rubricUrl;
+  newBadge.issuerUrl = badge.issuerUrl;
+  newBadge.timeValue = badge.timeValue;
+  newBadge.timeUnits = badge.timeUnits;
+  newBadge.limit = badge.limit;
+  newBadge.unique = badge.unique;
+  newBadge.criteria = badge.criteria;
+  newBadge.criteria.forEach(function(criterion) {
+    delete criterion.badgeId;
+    delete criterion.id;
+  });
+  
   return newBadge;
 };
