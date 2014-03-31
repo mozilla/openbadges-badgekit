693aa4a6e07154eb8fe0936d637861e937042200
Erik Christensen <christensenep@gmail.com>
2014-03-30 19:53:23 -0700

    Adding confirmation step

--- app/static/js/view-badge.js
+++ app/static/js/view-badge.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $('.js-delete-btn').click(function() {
+    $('.js-delete-confirm-text').show();
+  });
+
+  $('.js-delete-confirm-btn').click(function() {
     $.post(this.href, { _csrf: $(this).data('csrf') }, function(data) {
       window.location.href = data.location;
     });
